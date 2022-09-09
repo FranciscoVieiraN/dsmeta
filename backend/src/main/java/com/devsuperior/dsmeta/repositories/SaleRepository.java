d6ed85bc29c3c93e35a82382cb8f7655199e1d60
Francisco Vieira <francisco.assis@dcx.ufpb.br>
2022-09-09 16:49:02 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -11,6 +11,6 @@ import java.time.LocalDate;
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
 
-    //@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
-    //Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+    @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+    Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
